41d65defe18f2cfc9b94b5c29e95208a510ace65
ivan096096 <wanwanich@yandex.ru>
2022-12-17 23:40:22 +0300

    lesson_1

---
+++ src/main/java/lesson_1/Pow_Write.java
@@ -0,0 +1,20 @@
+package lesson_1;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Pow_Write {
+    public void computing(int a, int b) {
+        try (FileWriter fw = new FileWriter("output.txt", false)) {
+            if (a == 0 && b == 0) {
+                fw.write("Число не определённое");
+                fw.close();
+            } else {
+                fw.write(String.format("а = %d, b = %d, ответ: %f", a, b, Math.pow(a, b)));
+                fw.close();
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
