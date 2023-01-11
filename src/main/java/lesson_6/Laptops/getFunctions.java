4f1e2565b4c799d7220cbb2d6e64c47e0b4042a5
ivan096096 <wanwanich@yandex.ru>
2023-01-11 19:10:53 +0300

    lesson_6

---
+++ src/main/java/lesson_6/Laptops/getFunctions.java
@@ -0,0 +1,8 @@
+package lesson_6.Laptops;
+import java.util.List;
+import java.util.Map;
+
+public interface getFunctions {
+    abstract List<Laptop> getListLaptops();
+    abstract Map<Integer, String> getListFilters();
+}
